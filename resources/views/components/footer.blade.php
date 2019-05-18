5844eed57426c66360d7d2d4186b7f4f15bf986b
Jonathan Bell <jbell@benevity.com>
2019-05-17 21:13:14 -0700

    Update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,17 +3,17 @@
   <p>Other than boring old <a href="jonathanbell.ca+projects@gmail.com">email</a>, here are some different ways to get in touch:</p>
 
   <ul>
-    <li><a href="https://github.com/jonathanbell?utf8=%E2%9C%93&tab=repositories&q=&type=source">View my code on GitHub</a></li>
-    <li><a href="https://repl.it/@jonathanbell">Checkout my experiments on Repl.it</a> (be sure to click the "Run" button!)</li>
-    <li><a href="https://gist.github.com/jonathanbell">Read my makeshift tech blog</a></li>
-    <li><a href="http://cv.jonathanbell.ca" title="Boring.. I know..">My résumé</a></li>
-    <li><a href="https://codepen.io/jonathanbell/">I even have CodePens!</a></li>
+    <li><a rel="noopener" href="https://github.com/jonathanbell?utf8=%E2%9C%93&tab=repositories&q=&type=source">View my code on GitHub</a></li>
+    <li><a rel="noopener" href="https://repl.it/@jonathanbell">Checkout my experiments on Repl.it</a> (be sure to click the "Run" button!)</li>
+    <li><a rel="noopener" href="https://gist.github.com/jonathanbell">Read my makeshift tech blog</a></li>
+    <li><a rel="noopener" href="http://cv.jonathanbell.ca" title="Boring.. I know..">My résumé</a></li>
+    <li><a rel="noopener" href="https://codepen.io/jonathanbell/">I even have CodePens!</a></li>
   </ul>
 
   <p>
-    <a title="View this site's source code on GitHub" href="https://github.com/jonathanbell/projects.jonathanbell.ca">This site</a> is built with <a href="https://laravel.com/">Laravel</a>, the PHP framework that continues to facinate me to this day.
+    <a rel="noopener" title="View this site's source code on GitHub" href="https://github.com/jonathanbell/projects.jonathanbell.ca">This site</a> is built with <a href="https://laravel.com/" rel="noopener">Laravel</a>, the PHP framework that continues to facinate me to this day.
 
-    <svg xmlns="http://www.w3.org/2000/svg" id="laravel-logo" aria-label="Laravel logo" viewBox="0 0 700 700"><title>Laravel logo</title>
+    <svg xmlns="http://www.w3.org/2000/svg" id="laravel-logo" aria-label="Laravel logo" viewBox="0 0 700 700">
       <path fill="#fb503b" d="M556.8 344.2c-2.9-3.2-42.3-52.6-49.3-61-7-8.4-10.3-6.9-14.6-6.2s-54.2 9-60 10c-5.8 1-9.4 3.3-5.9 8.3 3.2 4.4 36 51 43.2 61.2l-130.5 31.3-103.6-173.5c-4.1-6.1-5-8.3-14.4-7.8-9.4.4-81.4 6.4-86.5 6.8-5.1.4-10.8 2.7-5.6 14.8 5.1 12.1 86.9 188.3 89.2 193.4 2.3 5.1 8.2 13.4 22.1 10.1 14.2-3.4 63.5-16.3 90.4-23.3 14.2 25.8 43.3 78 48.6 85.5 7.2 10 12.1 8.3 23.1 5 8.6-2.6 134.4-47.8 140.1-50.2 5.7-2.3 9.2-4 5.3-9.7-2.8-4.2-36-48.6-53.4-71.9 11.9-3.2 54.2-14.4 58.8-15.7 5.2-1.3 5.9-3.9 3-7.1zm-237.2 48.5c-1.6.3-74.6 17.8-78.5 18.7-3.9.9-3.9.4-4.3-.9-.4-1.3-86.9-179.6-88.1-181.8-1.2-2.2-1.1-4 0-4s69-6.1 71.4-6.2 2.1.4 3 1.9c0 0 95.7 165.2 97.4 168.1 1.7 3 .7 3.9-.9 4.2zm206.1 38.6c1.2 1.8 2.3 3-1.3 4.3-3.7 1.3-123.7 41.8-126 42.8-2.3 1-4.2 1.3-7.2-3s-42-71.9-42-71.9l127.2-33.1c3.2-1 4.2-1.7 6.2 1.5s41.9 57.6 43.1 59.4zm8.1-90.3c-2.9.6-49.6 12.2-49.6 12.2L446 300.8c-1.1-1.5-2-3 .7-3.4 2.7-.4 46.1-8.2 48-8.6 2-.4 3.7-1 6.1 2.3 2.4 3.3 35.4 45.1 36.7 46.7 1.3 1.6-.8 2.6-3.7 3.2z"/>
     </svg>
   </p>
